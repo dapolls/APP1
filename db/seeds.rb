d3cabff5338c6a74be1c12d92f709b730f527418
Sarah <sarah.dapollonia@gmail.com>
2016-04-11 15:15:24 +0200

    add product color and price

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create(:name => "seed-generated product", :description => "Product 2")
+Product.create(:name => "seed-generated product", :description => "Product 3")
