cb58de33b9e0898622ed3149a7b06fedd758586c
Sarah <sarah.dapollonia@gmail.com>
2016-04-25 18:11:22 +0200

    rating comments

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
 
   post 'static_pages/thank_you'
 
+  resources :products do
+    resources :comments
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
